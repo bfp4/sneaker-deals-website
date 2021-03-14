cecf5792d1ab34bac3fa694f3dfb79fd191a5e34
bfp4 <66973969+bfp4@users.noreply.github.com>
2021-03-14 11:56:16 -0400

    backend updated

---
+++ backend/scraper.js
@@ -0,0 +1,21 @@
+import nike from "./scrapers/nike.js"
+import adidas from "./scrapers/adidas.js"
+import end from "./scrapers/end.js"
+import finalscore from "./scrapers/finalscore.js"
+
+const d = new Date()
+const month = d.getMonth()
+const day = d.getDate()
+const year = d.getFullYear()
+const date = `${month}${day}${year}`
+
+function scrapeAll(){
+    nike(date)
+    adidas(date)
+    end(date)
+    finalscore(date)
+}
+
+scrapeAll()
+
+
---
+++ backend/scrapers/nike.js
@@ -0,0 +1,66 @@
+import puppeteer from "puppeteer"
+import useAddFirestore from "../hooks/useAddFirestore.js"
+
+export default async function nikeScraper(date){
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto("https://www.nike.com/w/sale-shoes-3yaepzy7ok");
+
+    const nikeData = []
+
+    const titles = await page.evaluate(() => {
+        const titles = document.querySelectorAll(".product-card__title")
+        const titleList = [...titles]
+        const text = titleList.map(title => title.innerText)
+        return text
+    })
+    titles.forEach((el, i) => {
+        nikeData[i] = {}
+        nikeData[i].title = el
+        nikeData[i].date = date
+        nikeData[i].website = "Nike"
+    })
+
+    const links = await page.evaluate(() => {
+        const links = document.querySelectorAll(".product-card__img-link-overlay")
+        const linksList = [...links]
+        const href = linksList.map(link => link.href)
+        return href
+    })
+    links.forEach((el, i) => {
+        nikeData[i].link = el
+    })
+
+    const prices = await page.evaluate(() => {
+        const prices = document.querySelectorAll(".product-price__wrapper")
+        const priceList = [...prices]
+        const text = priceList.map(price => price.innerText)
+        return text
+    })
+    prices.forEach((el, i) => {
+        const splitEl = el.split("\n")
+        const percent = Math.round((splitEl[0].split("$")[1] / splitEl[1].split("$")[1]) * 100)
+        nikeData[i].sale = splitEl[0]
+        nikeData[i].retail = splitEl[1]
+        nikeData[i].percent = percent
+    })
+
+    const images = await page.evaluate(() => {
+        const images = document.querySelectorAll("img")
+        const imageList = [...images]
+        const src = imageList.map(img => img.src).filter(src => src.includes("static.nike.com"))
+        return src
+    })
+    images.forEach((el, i) => {
+        nikeData[i].image = el
+    })
+
+    await browser.close();
+
+    for(let entry of nikeData){
+        useAddFirestore(entry)
+    }
+}
